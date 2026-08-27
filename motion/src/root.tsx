4869f181913cfbe530a40bac91153c0353e9aba9
mohdassen <46775336+mohdassen@users.noreply.github.com>
2026-08-27 20:35:18 +0300

    Register fast data story preview composition

--- motion/src/root.tsx
+++ motion/src/root.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {Composition} from 'remotion';
 import {HybridLiveAcceptance} from './hybridLiveAcceptance';
 import {StudioAcceptance} from './studioAcceptance';
+import {DataStoryPreview} from './dataStoryPreview';
 
 export const Root: React.FC = () => (
   <>
     <Composition id="HybridLiveAcceptance" component={HybridLiveAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
     <Composition id="StudioAcceptance" component={StudioAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
+    <Composition id="DataStoryPreview" component={DataStoryPreview} durationInFrames={180} fps={30} width={1080} height={1920} />
   </>
 );
