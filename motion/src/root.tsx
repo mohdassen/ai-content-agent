42dada1ba5289ad91f1b9e0ed06c6e4f6da614d2
mohdassen <46775336+mohdassen@users.noreply.github.com>
2026-08-27 16:03:47 +0300

    Register isolated presenter studio composition

--- motion/src/root.tsx
+++ motion/src/root.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {HybridLiveAcceptance} from './hybridLiveAcceptance';
+import {StudioAcceptance} from './studioAcceptance';
 
 export const Root: React.FC = () => (
-  <Composition
-    id="HybridLiveAcceptance"
-    component={HybridLiveAcceptance}
-    durationInFrames={270}
-    fps={30}
-    width={1080}
-    height={1920}
-  />
+  <>
+    <Composition id="HybridLiveAcceptance" component={HybridLiveAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
+    <Composition id="StudioAcceptance" component={StudioAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
+  </>
 );
