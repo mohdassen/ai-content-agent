a41c5e15680c7116f97ab422d7d352f4e774534a
mohdassen <46775336+mohdassen@users.noreply.github.com>
2026-08-27 21:04:04 +0300

    Register seamless master explainer

--- motion/src/root.tsx
+++ motion/src/root.tsx
@@ -3,11 +3,13 @@ import {Composition} from 'remotion';
 import {HybridLiveAcceptance} from './hybridLiveAcceptance';
 import {StudioAcceptance} from './studioAcceptance';
 import {DataStoryPreview} from './dataStoryPreview';
+import {MasterExplainer} from './masterExplainer';
 
 export const Root: React.FC = () => (
   <>
     <Composition id="HybridLiveAcceptance" component={HybridLiveAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
     <Composition id="StudioAcceptance" component={StudioAcceptance} durationInFrames={270} fps={30} width={1080} height={1920} />
     <Composition id="DataStoryPreview" component={DataStoryPreview} durationInFrames={180} fps={30} width={1080} height={1920} />
+    <Composition id="MasterExplainer" component={MasterExplainer} durationInFrames={840} fps={30} width={1080} height={1920} />
   </>
 );
